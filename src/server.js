ec492d67803067158e810e00e817b20d14cc39b6
shmck <shawn.j.mckay@gmail.com>
2020-03-16 20:25:15 -0700

    3.1 solution send html file

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 const server = app.listen(process.env.PORT || 3000);
