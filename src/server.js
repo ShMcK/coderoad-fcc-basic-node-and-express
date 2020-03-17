ce2099d5797d44363faaa7ec2c8c385387c9c900
shmck <shawn.j.mckay@gmail.com>
2020-03-16 20:10:46 -0700

    2.1 solution get root

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 console.log("Hello World");
 
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
 const server = app.listen(process.env.PORT || 3000);
 
 // -- DO NOT EDIT BELOW THIS LINE
