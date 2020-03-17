b16b05d1c408798fb094d40ad196973f436e983f
shmck <shawn.j.mckay@gmail.com>
2020-03-16 21:05:19 -0700

    5.1 solution json message

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+
 const server = app.listen(process.env.PORT || 3000);
 
 // -- DO NOT EDIT BELOW THIS LINE
