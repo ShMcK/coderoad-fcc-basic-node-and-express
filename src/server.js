79d3c1c06def9235181e4a67ab51e6d1681eb34f
shmck <shawn.j.mckay@gmail.com>
2020-03-16 19:53:01 -0700

    1.2 solution console.log

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+console.log("Hello World");
+
 const server = app.listen(process.env.PORT || 3000);
 
 // -- DO NOT EDIT BELOW THIS LINE
