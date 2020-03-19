5371d9f9c4895ffeadbbe956c05f18d61d044667
shmck <shawn.j.mckay@gmail.com>
2020-03-18 21:09:02 -0700

    9.1 solution echo word params

--- src/server.js
+++ src/server.js
@@ -36,6 +36,11 @@ app.get(
   }
 );
 
+app.get("/echo/:word", (req, res) => {
+  const { word } = req.params;
+  res.json({ echo: word });
+});
+
 const server = app.listen(process.env.PORT || 3000);
 
 // -- DO NOT EDIT BELOW THIS LINE
